d746cd813fdf0f2483d196047ca92a2785d88ced
BenjaminKlatt <benjamin.klatt@viadee.de>
2021-02-26 23:09:49 +0100

    Example service app base test

---
+++ calculator-service/src/test/java/de/viadee/camunda/connected/calculator/CalculatorServiceApplicationTest.java
@@ -0,0 +1,13 @@
+package de.viadee.camunda.connected.calculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalculatorServiceApplicationTest {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
