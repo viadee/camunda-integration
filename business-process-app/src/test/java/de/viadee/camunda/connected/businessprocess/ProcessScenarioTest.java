2d87d0c992e2b2003ec8281b26de546fcb50c45b
Matthias Schulte <matthias.schulte@viadee.de>
2021-03-24 11:20:23 +0100

    getting junit5 tests to work with maven

--- business-process-app/src/test/java/de/viadee/camunda/connected/businessprocess/ProcessScenarioTest.java
+++ business-process-app/src/test/java/de/viadee/camunda/connected/businessprocess/ProcessScenarioTest.java
@@ -1,30 +1,29 @@
 package de.viadee.camunda.connected.businessprocess;
 
-import static org.camunda.bpm.engine.test.assertions.bpmn.BpmnAwareTests.assertThat;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.mockito.Mockito.*;
-
-import java.util.Map;
-
 import de.viadee.camunda.connected.businessprocess.calculator.Operator;
 import org.apache.ibatis.logging.LogFactory;
 import org.camunda.bpm.engine.test.Deployment;
 import org.camunda.bpm.engine.variable.Variables;
-import org.camunda.bpm.extension.junit5.test.ProcessEngineExtension;
 import org.camunda.bpm.scenario.ProcessScenario;
 import org.camunda.bpm.scenario.Scenario;
-import org.camunda.bpm.scenario.run.ProcessRunner.ExecutableRunner.StartingByKey;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.util.Map;
+
+import static org.camunda.bpm.engine.test.assertions.bpmn.BpmnAwareTests.assertThat;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.Mockito.*;
 
 /**
  * Test case starting an in-memory database-backed Process Engine.
  */
-@ExtendWith(ProcessEngineExtension.class)
+@ExtendWith(SpringExtension.class)
 @SpringBootTest
 public class ProcessScenarioTest {
 
@@ -33,26 +32,29 @@ public class ProcessScenarioTest {
     static {
         LogFactory.useSlf4jLogging(); // MyBatis
     }
-    
+
     @BeforeEach
     public void setup() {
-      MockitoAnnotations.initMocks(this);
-     when(myScenario.waitsAtUserTask("utErgebnisBestaetigen")).thenReturn((task) -> {task.complete();});
+        MockitoAnnotations.initMocks(this);
+        when(myScenario.waitsAtUserTask("utErgebnisBestaetigen")).thenReturn((task) -> {
+            task.complete();
+        });
     }
-    
-    @Mock private ProcessScenario myScenario;
-    
+
+    @Mock
+    private ProcessScenario myScenario;
+
     @Test
     @Deployment(resources = {"process.bpmn"})
     public void testPlainDelegateIntegration() {
 
-        Map<String, Object> variables = 
-            Variables.createVariables()
-          .putValue("integrationType", "plain")
-          .putValue("num1", Long.valueOf("10"))
-          .putValue("num2", Long.valueOf("10"))
-          .putValue("operator", Operator.Plus.toString());
-      
+        Map<String, Object> variables =
+                Variables.createVariables()
+                        .putValue("integrationType", "plain")
+                        .putValue("num1", Long.valueOf("10"))
+                        .putValue("num2", Long.valueOf("10"))
+                        .putValue("operator", Operator.Plus.toString());
+
         Scenario scenario = Scenario.run(myScenario).startByKey(PROCESS_DEFINITION_KEY, variables).execute();
         assertNotNull(scenario);
 
--- calculator-service/src/test/java/de/viadee/camunda/connected/calculator/CalculatorServiceApplicationTest.java
+++ calculator-service/src/test/java/de/viadee/camunda/connected/calculator/CalculatorServiceApplicationTest.java
@@ -1,13 +1,25 @@
 package de.viadee.camunda.connected.calculator;
 
+import de.viadee.camunda.connected.calculator.service.CalculatorService;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+@ExtendWith(SpringExtension.class)
 @SpringBootTest
-class CalculatorServiceApplicationTest {
+public class CalculatorServiceApplicationTest {
+
+  @Autowired
+  private CalculatorService service;
 
   @Test
-  void contextLoads() {
+  public void testContextLoads() {
+    assertNotNull(service);
   }
+
 
 }
