68c6c2005459290697a09da96bc9bc59e88e7e57
EnnoKm <enno.knollmeyer@viadee.de>
2021-04-07 14:32:24 +0200

    Changes to MiniCalculator (switch instead of else if, Operater instead of String as Paramenter) & according changes to CalculatorDelegate to feed the correct Operator into the MiniCalculator

--- business-process-app/src/main/java/de/viadee/camunda/connected/businessprocess/calculator/MiniCalculator.java
+++ business-process-app/src/main/java/de/viadee/camunda/connected/businessprocess/calculator/MiniCalculator.java
@@ -10,18 +10,23 @@ public class MiniCalculator {
 
     private final Logger LOG = LoggerFactory.getLogger(MiniCalculator.class);
 
-	public Number processCalculation(long number1, String operator, long number2) {
-    	if (Operator.Plus.toString().equals(operator)) {
-			return number1 + number2;
-    	} else if (Operator.Minus.toString().equals(operator)) {
-    		return number1 - number2;
-    	} else if (Operator.Mal.toString().equals(operator)) {
-    		return number1 * number2;
-    	} else if (Operator.Geteilt.toString().equals(operator)) {
-    		return new BigDecimal(number1).divide(new BigDecimal(number2), 2, RoundingMode.CEILING);
-    	} else {
-    		LOG.warn("Invalid operator: {}", operator);
-    		return 0;
-		}
-	}
+    public Number processCalculation(long number1, Operator operator, long number2) {
+        switch (operator) {
+            case Plus:
+                return number1 + number2;
+                
+            case Minus:
+                return number1 - number2;
+
+            case Mal:
+                return number1 * number2;
+
+            case Geteilt:
+                return new BigDecimal(number1).divide(new BigDecimal(number2), 2, RoundingMode.HALF_UP);
+
+            default:
+                LOG.warn("Invalid operator: {}", operator);
+                return 0;
+        }
+    }
 }
--- business-process-app/src/main/java/de/viadee/camunda/connected/businessprocess/delegate/CalculatorDelegate.java
+++ business-process-app/src/main/java/de/viadee/camunda/connected/businessprocess/delegate/CalculatorDelegate.java
@@ -7,6 +7,7 @@ import org.camunda.bpm.engine.variable.value.StringValue;
 import org.springframework.stereotype.Component;
 
 import de.viadee.camunda.connected.businessprocess.calculator.MiniCalculator;
+import de.viadee.camunda.connected.businessprocess.calculator.Operator;
 
 /**
  * Plain Old Java Delegate for a calculation facilitating an embedded calculator
@@ -21,11 +22,11 @@ public class CalculatorDelegate implements JavaDelegate {
 
 		// Input
 		LongValue number1 = execution.getVariableTyped("num1");
-		StringValue operator = execution.getVariableTyped("operator");
 		LongValue number2 = execution.getVariableTyped("num2");
-
+		Operator operator = Operator.valueOf(execution.getVariableTyped("operator").getValue().toString());
+		
 		// Process
-		Number result = calculator.processCalculation(number1.getValue(), operator.getValue(), number2.getValue());
+		Number result = calculator.processCalculation(number1.getValue(), operator, number2.getValue());
 
 		// Output
 		execution.setVariable("result", result);
